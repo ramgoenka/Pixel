54ba5406af4e3bfe42c8d15749b15e2fee266faa
Ram Goenka <98141763+RamGoenka@users.noreply.github.com>
2023-01-05 15:26:48 +0400

    Added integral functionality

--- MathCommands.py
+++ MathCommands.py
@@ -1,41 +1,51 @@
-#command prefix is 'pmath'
+#command prefix is 'pmat'
+
+#INTEGRAL CALCULATOR
+    if message.content.startswith('pmathintegral'):
+        try:
+            words = message.content.split(None, 1)
+            x = symbols('x')
+            result = integrate(words[1], x)
+            await message.channel.send(f"Result: ```{result}```")
+        except IndexError:
+            await message.channel.send("No function to integrate. Please enter a function to integrate and try again! Use ``p;info integral`` to learn how to use this feature.")
 
 @client.command()
-async def add(ctx, num1: int, num2: int):
+async def hadd(ctx, num1: int, num2: int):
   a = num1+num2
   await ctx.send(f"**Result:** {a}")
 
 @client.command()
-async def substract(ctx, num1: int, num2: int):
+async def hsubstract(ctx, num1: int, num2: int):
   a = num1-num2
   await ctx.send(f"**Result:** {a}")
 
 @client.command()
-async def multiply(ctx, num1: int, num2: int):
+async def hmultiply(ctx, num1: int, num2: int):
   a = num1*num2
   await ctx.send(f"**Result:** {a}")  
 
 @client.command()
-async def divide(ctx, num1: int, num2: int):
+async def hdivide(ctx, num1: int, num2: int):
   a = num1/num2
   await ctx.send(f"**Result:** {a}")
 
 @client.command()
-async def exp(ctx, num1: int, num2: int):
+async def hexp(ctx, num1: int, num2: int):
   a = num1**num2
   await ctx.send(f"**Result:** {a}")
 
 @client.command()
-async def sqrt(ctx, num1: int):
+async def hsqrt(ctx, num1: int):
   a = num1**0.5
   await ctx.send(f"**Result:** {a}")  
 
 @client.command()
-async def factorial(ctx, num1: int):
+async def hfactorial(ctx, num1: int):
   a = math.factorial(num1)
   await ctx.send(f"**Result:** {a}") 
 
 @client.command()
-async def log(ctx, num1: int, num2: int):
+async def hlog(ctx, num1: int, num2: int):
   a = math.log(num1, num2)
   await ctx.send(f"**Result:** {a}")
