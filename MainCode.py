fa9a4af93a4c12eaed9f1d57656bb43852342a1e
Ram Goenka <98141763+RamGoenka@users.noreply.github.com>
2023-01-07 18:41:48 +0400

    Update MainCode.py

--- MainCode.py
+++ MainCode.py
@@ -933,6 +933,8 @@ async def place_error(ctx, error):
         await ctx.send("Please enter a position you would like to mark.")
     elif isinstance(error, commands.BadArgument):
         await ctx.send("Please make sure to enter an integer.")
+        
+#--LISTS--
 
 embed_footers = [
     'Thanks for using pixel :)',
