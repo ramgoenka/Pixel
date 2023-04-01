1d9438972d319f383a4d6eaca72fb50c2a8fc1b8
Ram Goenka <98141763+RamGoenka@users.noreply.github.com>
2023-04-01 03:27:18 -0500

    Minor changes

--- keepalive.py
+++ keepalive.py
@@ -1,16 +1,17 @@
-#THIS FILE IS USED TO KEEP THE BOT ALIVE (ONLINE) FOR 24/7
+#THIS FILE IS USED TO KEEP THE BOT ALIVE (ONLINE) 24/7
 from flask import Flask
 from threading import Thread 
-
 app = Flask('')
 
 @app.route('/')
+
 def home():
-  return "Bot Status: Online."
+  return "The bot is currently online."
 
 def run():
   app.run(host='0.0.0.0', port=8080)
-
+  
 def keep_alive():
     t = Thread(target=run)
     t.start()
+    
